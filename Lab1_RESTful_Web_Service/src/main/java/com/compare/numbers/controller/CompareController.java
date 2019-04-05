f0d9806dec23fe75eedbd750723f33e6fd30df6d
Tsimofei Sikorski <sikorskitimofei@gmail.com>
2019-04-05 21:01:11 +0300

    remove static, add @Autowired

--- Lab1_RESTful_Web_Service/src/main/java/com/compare/numbers/controller/CompareController.java
+++ Lab1_RESTful_Web_Service/src/main/java/com/compare/numbers/controller/CompareController.java
@@ -4,6 +4,7 @@ import com.compare.numbers.entity.ComparableNumbers;
 import com.compare.numbers.entity.CompareResult;
 import com.compare.numbers.service.CompareService;
 import com.compare.numbers.utilites.ConvertUtility;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,19 @@ import java.util.ArrayList;
 
 @RestController
 public class CompareController {
+    private CompareService service;
+
+    @Autowired
+    public CompareController(CompareService service) {
+        this.service = service;
+    }
+
     @RequestMapping("/compareNumber")
     public CompareResult greeting(@RequestParam(value="numbers", defaultValue="0") String numbers) {
 
         ArrayList<Integer> input = ConvertUtility.convertStringToInt(numbers);
 
         ComparableNumbers comparableNumbers = new ComparableNumbers(input);
-        return CompareService.getMaximumNumber(comparableNumbers);
+        return service.getMaximumNumber(comparableNumbers);
     }
 }
--- Lab1_RESTful_Web_Service/src/main/java/com/compare/numbers/service/CompareService.java
+++ Lab1_RESTful_Web_Service/src/main/java/com/compare/numbers/service/CompareService.java
@@ -2,11 +2,13 @@ package com.compare.numbers.service;
 
 import com.compare.numbers.entity.ComparableNumbers;
 import com.compare.numbers.entity.CompareResult;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 
+@Service
 public class CompareService {
-    public static CompareResult getMaximumNumber(ComparableNumbers numbers) {
+    public CompareResult getMaximumNumber(ComparableNumbers numbers) {
 
         Collections.sort(numbers.getComparableNumbers(),Collections.reverseOrder());
         int maximumNumber = numbers.getComparableNumbers().get(0);
